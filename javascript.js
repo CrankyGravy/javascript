d1fbd4adecfefd1c252e3f2f056b955b953cf124
David <davidgreene95@gmail.com>
2023-10-15 14:29:55 +0100

    Added capitalize function

--- javascript.js
+++ javascript.js
@@ -10,3 +10,9 @@ function multiply(a, b) {
 }
 
 console.log(multiply(7, 7));
+
+function capitalize(myString = undefined) {
+  return myString[0].toUpperCase() + myString.slice(1).toLowerCase();
+}
+
+console.log(capitalize("hElLO"));
